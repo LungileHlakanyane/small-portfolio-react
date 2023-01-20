3ab7b398ca138cbdc56a458ed2832dc44fc9955f
LungileHlakanyane <82327071+LungileHlakanyane@users.noreply.github.com>
2023-01-20 02:09:59 +0200

    File uploads

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React, { userState } from 'react';
+
+function Home() {
+    return ( <
+        div >
+        <
+        h1 > About Me < /h1> <p> 
+        I am Lungile Hlakanyane and I have a passion
+        for technology, I recently completed my studies
+        for IT majoring in software development.I am always willing to learn new skills and technologies.I am able to work independently in a busy environment and also within a team setting.I am well organized and always plan ahead to make sure I make my time well organized. <
+        /p > <
+        h2 > Why I want to be a developer at SovTech < /h2> <
+        p > Throughout my studies, I have gained a strong foundation in software development concepts and techniques, including object - oriented programming, database management, and web development.I have also had the opportunity to work on several projects, both individually and as part of a team, which have allowed me to develop my problem - solving, communication, and collaboration skills.
+
+        I am particularly interested in SovTech because of its reputation
+        for producing high - quality software products and services and its commitment to innovation and excellence.I am excited about the opportunity to work with a team of experienced software engineers and contribute to the development of cutting - edge software solutions.
+
+        I am confident that my technical skills, as well as my passion
+        for software engineering, make me an excellent candidate
+        for the Graduate Program.I am eager to learn and grow as a software engineer, and I believe that SovTech is the perfect place to do so. < /p>  <
+        p > below is a link
+        for my porfolio to get a clear picture of me < /p> <
+        a href = "https://lungilehlakanyane.github.io/portfolio/" > My Portfolio < /a>
+
+
+        <
+        /div>
+
+
+    );
+}
+
+export default Home;
